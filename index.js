0d3dff3980e9297d986578445e66a7080e23a865
mayurlakhani <mayur22lakhani33@gmail.com>
2023-03-02 23:43:02 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+console.log("hi hello");
+
+// comment
+
+/*
+    multi line comment
+*/
+
+
+//Variable
+
+let age = 21;
+age = age + 1;
+console.log(age);
+
+const pi = 3.14
+
+
+// DataType
+console.log(typeof age);
+
+
+// One dataType to another conversion
+age = Number(age);
+age =String(age);
+age = Boolean(age);
+
+// User Input
+let username = window.prompt("Enter user name:");
+console.log("your name is", username);
